e387bcdcbec3b4b6e2d29bc684cf10aa3c11d724
bfartsov@htv.bg <bfartsov@htv.bg>
2020-05-08 17:19:56 +0300

    test

--- api/routes/api/__test__/menu.test.js
+++ api/routes/api/__test__/menu.test.js
@@ -153,6 +153,88 @@ describe("menus", () => {
         expect(res.status).toBe(200);
         expect(res.body.subMenu.some((m) => m.name === "subMenu")).toBeTruthy();
       });
+      test("it shoud return 400 if name is not provided", async () => {
+        const mainMenu = new Menu({ name: "menu1", order: 1, subMenu: [] });
+        await mainMenu.save();
+
+        const res = await request(app)
+          .post("/api/menus")
+          .set("x-auth-token", token)
+          .send({ order: 1, parentElement: "menu1" });
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBeTruthy();
+      });
+    });
+  });
+  describe("put", () => {
+    describe("edit main menu", () => {
+      beforeEach(async () => {
+        const user = await User.create({ email: "a@a.com", password: "hello" });
+        token = newToken(user);
+      });
+      test("it shuld successfuly edit main menu item if token and all required filed are present", async () => {
+        const menu = new Menu({ name: "menu1", order: 1, subMenu: [] });
+        await menu.save();
+        const res = await request(app)
+          .put("/api/menus/" + menu._id)
+          .set("x-auth-token", token)
+          .send({
+            name: "newMenu",
+            order: 2,
+            subMenu: [],
+            parentElement: "topLevel",
+          });
+        expect(res.status).toBe(200);
+        const item = await Menu.findById(menu._id);
+        expect(item).toHaveProperty("name", "newMenu");
+      });
+      test("it shuld return 400 if one of the require filed is not provided", async () => {
+        const menu = new Menu({ name: "menu1", order: 1, subMenu: [] });
+        await menu.save();
+        const res = await request(app)
+          .put("/api/menus/" + menu._id)
+          .set("x-auth-token", token)
+          .send({
+            order: 2,
+            subMenu: [],
+            parentElement: "topLevel",
+          });
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBeTruthy();
+      });
+      test("it shour return 400 if invalid id is provided", async () => {
+        const menu = new Menu({ name: "menu1", order: 1, subMenu: [] });
+        await menu.save();
+        const res = await request(app)
+          .put("/api/menus/2")
+          .set("x-auth-token", token)
+          .send({
+            order: 2,
+            subMenu: [],
+            parentElement: "topLevel",
+          });
+        expect(res.status).toBe(404);
+        expect(res.body.error).toBeTruthy();
+      });
+    });
+    describe("put subMenu", () => {
+      beforeEach(async () => {
+        const user = await User.create({ email: "a@a.com", password: "hello" });
+        token = newToken(user);
+      });
+      test("it shoud edit subMenu if token and all required fields are proided", async () => {
+        const menu = new Menu({
+          name: "menu1",
+          order: 1,
+          subMenu: [{ name: "subMenu", order: 1 }],
+        });
+        await menu.save();
+        const res = await request(app)
+          .put("/api/menus/" + menu.subMenu[0]._id)
+          .set("x-auth-token", token)
+          .send({ name: "newSubMenu", order: 2, parentElement: "menu1" });
+        expect(res.status).toBe(200);
+      });
     });
   });
 });
